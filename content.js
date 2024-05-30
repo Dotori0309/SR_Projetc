ef2d82107136e8960062942635a857c5a052fbb3
ICE0208 <doky3307@naver.com>
2024-05-30 19:38:01 +0900

    캐릭터 추가

--- content.js
+++ content.js
@@ -13,8 +13,6 @@ const removeResource = () => {
 
     // div에 스타일을 적용합니다.
     div.style.backgroundColor = "gray";
-    //   div.style.width = "100%";
-    //   div.style.height = "100%";
 
     // img 태그와 같은 위치에 div를 삽입합니다.
     img.parentNode.insertBefore(div, img);
@@ -36,4 +34,26 @@ const removeResource = () => {
     // 비디오를 다시 로드하여 변경사항을 적용합니다.
     video.load();
   });
+
+  // video-container_PC 클래스를 가진 요소 안에 이미지를 추가합니다.
+  const videoContainer = document.querySelector(".video-container_PC");
+  if (videoContainer) {
+    const img = document.createElement("img");
+    img.src =
+      "https://lms.sunmoon.ac.kr/ilosfiles/editor-file/795851C8337F5B52C933/2024/6E2AC53BY52IA.png";
+    img.style.position = "absolute";
+    img.style.bottom = "23%"; // bottom으로 위치 설정
+    img.style.left = "300px";
+    img.style.zIndex = "9999"; // 최대 z-index 값
+    img.style.width = "600px"; // 필요에 따라 조정
+    img.style.height = "auto"; // 필요에 따라 조정
+
+    videoContainer.appendChild(img);
+
+    // video 태그의 배경색을 초록색으로 변경합니다.
+    const video = videoContainer.querySelector("video");
+    if (video) {
+      video.style.backgroundColor = "#6caa6c"; // 배경색을 초록색으로 변경
+    }
+  }
 };
